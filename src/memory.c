2255fd9765931bdc3080bf3a42a82e9c34f883bb
Francisco Teixeira <franjateixeira@gmail.com>
2022-04-07 15:16:08 +0100

    Adicionado os cabeçalhos nos ficheiros .c

--- src/memory.c
+++ src/memory.c
@@ -1,3 +1,10 @@
+/*
+Grupo: SO-010
+Membros:
+Francisco Teixeira | FC56305
+Andre Alves        | FC56349
+Rodrigo Antunes    | FC56321
+*/
 #include <stdlib.h>
 #include <string.h>
 #include <unistd.h>
--- src/restaurant.c
+++ src/restaurant.c
@@ -1,17 +1,15 @@
+/*
+Grupo: SO-010
+Membros:
+Francisco Teixeira | FC56305
+Andre Alves        | FC56349
+Rodrigo Antunes    | FC56321
+*/
 #include <stdio.h>
 #include <string.h>
 #include "main.h"
 #include "memory.h"
 #include "restaurant.h"
-/* Função principal de um Restaurante. Deve executar um ciclo infinito onde em
-* cada iteração lê uma operação da main e se e data->terminate ainda for igual a 0, processa-a e
-* escreve-a para os motoristas. Operações com id igual a -1 são ignoradas
-* (op inválida) e se data->terminate for igual a 1 é porque foi dada ordem
-* de terminação do programa, portanto deve-se fazer return do número de
-* operações processadas. Para efetuar estes passos, pode usar os outros
-* métodos auxiliares definidos em restaurant.h.
-*/
-
 
 int execute_restaurant(int rest_id, struct communication_buffers* buffers, struct main_data* data){
     int ops = 0;
