a4ebe72d8fd2e1de3bb714fdac458001414eb1d6
andre <fc56349@alunos.fc.ul.pt>
2022-04-07 10:31:11 +0100

    correção de erro no launch_processes

--- src/driver.c
+++ src/driver.c
@@ -6,20 +6,21 @@
 
 int execute_driver(int driver_id, struct communication_buffers* buffers, struct main_data* data){
     int ops = 0;
+    int* ptr = &ops;
     int terminate_flag = *(data->terminate);
     struct operation* op = create_dynamic_memory(sizeof (struct operation));
     while (1){
         terminate_flag = *(data->terminate);
         if(terminate_flag == 1) {
             destroy_dynamic_memory(op);
-            return ops;
+            return *ptr;
         }
         else{
             memset(op,0,sizeof (struct operation));
             op->id=-1;
             driver_receive_operation(op, buffers, data);
-            if(op->id != -1){
-                driver_process_operation(op, driver_id, data, &ops);
+            if((op->id != -1)&&(op->status=='R')){
+                driver_process_operation(op, driver_id, data, ptr);
                 driver_send_answer(op, buffers, data);
             }
 
@@ -32,7 +33,7 @@ void driver_receive_operation(struct operation* op, struct communication_buffers
     if(terminate_flag == 1)
         return;
     else{
-        read_rest_driver_buffer(buffers->rest_driv, data->max_ops, op);
+        read_rest_driver_buffer(buffers->rest_driv, data->buffers_size, op);
 
     }
 }
@@ -46,5 +47,5 @@ void driver_process_operation(struct operation* op, int driver_id, struct main_d
 }
 
 void driver_send_answer(struct operation* op, struct communication_buffers* buffers, struct main_data* data){
-    write_driver_client_buffer(buffers->driv_cli, data->max_ops, op);
+    write_driver_client_buffer(buffers->driv_cli, data->buffers_size, op);
 }
